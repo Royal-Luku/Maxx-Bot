40982226b4091fd0c716c6e1fab505e1791f0dee
Sagar Bhattacharjee <137499419+sbplays10@users.noreply.github.com>
2023-08-01 18:19:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/sbplays10/SearchBot.git /SearchBot
+git clone https://github.com/sbplays10/SearchBot.git/SearchBot
 cd /SearchBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
